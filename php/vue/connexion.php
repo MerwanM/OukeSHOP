0f223293a62653d57a42bbb3064cae9e26e76b0b
Merwan <merwanwest@gmail.com>
2016-02-26 15:44:17 +0100

    Transfert

--- php/vue/connexion.php
+++ php/vue/connexion.php
@@ -72,83 +72,148 @@
           </div>
         </div>
 
-          <form id="connect" method="post" onsubmit="this.action=get_url_submit();">
-         <div class="row">
-          <div class="col-md-offset-2 col-md-3">
-           <div class="form-group">
-              <label for="Email">Votre e-mail :</label>
-              <input type="text" class="form-control" id="email" placeholder="Compte" required>
-            </div>
-         </div>
-          <div class="col-md-offset-1 col-md-3">
-              <label for="Password">Votre mot de passe :</label>
-              <input type="password" class="form-control" id="password" placeholder="********" required>
+          form name="form" action="connexion.php" method="POST">
+          <div class="row">
+              <div class="col-md-offset-2 col-md-3">
+                  <div class="form-group">
+                      <label for="Email">Votre email :</label>
+                      <input type="text" class="form-control" id="email" placeholder="Email">
+                  </div>
+              </div>
+              <div class="col-md-offset-1 col-md-3">
+                  <label for="Password">Password</label>
+                  <input type="password" class="form-control" id="password" placeholder="Password">
+              </div>
           </div>
-        </div>
-        </form>
 
 
-        <div class="row">
-          <div class="col-md-offset-2 col-md-3">
-            <button type="submit" class="btn btn-primary">Connexion</button>
+          <div class="row">
+              <div class="col-md-offset-2 col-md-3">
+                  <button type="submit" class="btn btn-primary">Connexion</button>
+              </div>
           </div>
-        </div>
       </div>
     </div>
-  
-  </br>
-  </br>
-  <hr>
-  </br>
+
+</br>
+</br>
+<hr>
+</br>
 
 
-    <div class="container">
-      <div class="wall">
-        
-        <div class="row">
-          <div class="col-md-offset-2 col-md-10">
-              <h1>Inscription</br><small>Merci de renseigner les informations suivantes</small></h1>
-          </div>
-        </div>
+<div class="container">
+    <div class="wall">
 
         <div class="row">
-          <div class="col-md-offset-2 col-md-3">
-           <div class="form-group">
-              <label for="Nom">Votre nom :</label>
-              <input type="text" class="form-control" id="nom" placeholder="Nom">
+            <div class="col-md-offset-2 col-md-10">
+                <h1>Page d'inscription</br><small>Merci de renseigner vos informations</small></h1>
             </div>
-         </div>
-          <div class="col-md-offset-1 col-md-3">
-           <div class="form-group">
-              <label for="Prenom">Votre pr&eacute;nom :</label>
-              <input type="text" class="form-control" id="prenom" placeholder="Pr&eacute;nom" pattern="[A-Za-z]+">
-            </div>
-         </div>
         </div>
 
-      <div class="row">
-          <div class="col-md-offset-2 col-md-7">
-              <div class="form-group">
-                <label for="Email">Entrez votre adresse e-mail :</label>
-                <input type="text" class="form-control" id="mail" placeholder="e-mail">
-              </div>
-         </div>
-          <div class="col-md-offset-2 col-md-3">
-              <div class = "form-group">
-                <label for="Password">Votre mot de passe :</label>
-                  <input type="password" class="form-control" id="password" placeholder="************">
-             </div>
+        <div class="row">
+            <div class="col-md-offset-2 col-md-3">
+                <div class="form-group">
+                    <label for="Nom">Nom</label>
+                    <input type="text" class="form-control" id="nom" placeholder="Nom">
+                </div>
+            </div>
+            <div class="col-md-offset-1 col-md-3">
+                <div class="form-group">
+                    <label for="Prenom">Pr&eacute;nom</label>
+                    <input type="text" class="form-control" id="prenom" placeholder="Prenom">
+                </div>
+            </div>
         </div>
-      </div>
 
-    </br>
-      <div class ="row">
-           <div class="col-md-offset-2 col-md-3">
-                   <button type="submit" class="btn btn-primary">Valider</button>
-           </div>
-       </div>
-      </div>
+        <div class="row">
+            <div class="col-md-offset-2 col-md-7">
+                <div class="form-group">
+                    <label for="Email">Email</label>
+                    <input type="text" class="form-control" id="email" placeholder="Email">
+                </div>
+            </div>
+        </div>
 
+        <div class="row">
+            <div class="col-md-offset-2 col-md-7">
+                <div class="form-group">
+                    <label for="VEmail">V&eacute;rification Email</label>
+                    <input type="text" class="form-control" id="Vemail" placeholder="V&eacute;rification Email">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-offset-2 col-md-3">
+                <label for="Password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+            <div class="col-md-offset-1 col-md-3">
+                <label for="VPassword">V&eacute;rification Password</label>
+                <input type="password" class="form-control" id="Vpassword" placeholder="V&eacute;rification Password">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-offset-2 col-md-7">
+                <div class="form-group">
+                    <label for="Adresse">Adresse</label>
+                    <input type="text" class="form-control" id="Adresse" placeholder="Adresse">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-offset-2 col-md-7">
+                <div class="form-group">
+                    <label for="CPAdresse">Cp. d'Adresse</label>
+                    <input type="text" class="form-control" id="CPAdresse" placeholder="Compl&eacute;ment d'Adresse">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-offset-2 col-md-3">
+                <div class="form-group">
+                    <label for="CP">Code Postal</label>
+                    <input type="text" class="form-control" id="CP" placeholder="Code Postal" pattern="[0-9]{5}">
+                </div>
+            </div>
+            <div class="col-md-offset-1 col-md-3">
+                <div class="form-group">
+                    <label for="Ville">Ville</label>
+                    <input type="text" class="form-control" id="Ville" placeholder="Ville">
+                </div>
+            </div>
+        </div>
+
+
+        </br>
+
+        <div class="row">
+            <div class="col-md-offset-2 col-md-3">
+                <div class="input-group">
+                    <span class="input-group-addon glyphicon glyphicon-phone"> </span>
+                    <input type="text" class="form-control" id="tel" placeholder="Numéro de mobile">
+                </div>
+            </div>
+
+            <div class="col-md-offset-1 col-md-3">
+                <div class="input-group">
+                    <span class="input-group-addon glyphicon glyphicon-globe"> </span>
+                    <input type="text" class="form-control" id="adresse" placeholder="Adresse">
+                </div>
+            </div>
+        </div>
+
+        </br>
+        <div class="row">
+            <div class="col-md-offset-2 col-md-3">
+                <button type="submit" class="btn btn-primary">Inscription</button>
+            </div>
+        </div>
+
+        </form>
 
       </div>
     </div>
